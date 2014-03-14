033c80b6784dae8b3afd5c21edad91e9f2f4ab35
alex <Root1@192.168.186.130>
2014-03-14 06:03:39 -0700

    Initial Commit

---
+++ firstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firstApp
+//
+//  Created by alex on 14-3-14.
+//  Copyright (c) 2014年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
